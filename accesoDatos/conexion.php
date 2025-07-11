57148bb29320c6df4c35afde06a94eb8d3475217
caleb1004 <jcbustosg10@gmail.com>
2025-07-10 22:02:58 -0600

    Conexion

--- accesoDatos/conexion.php
+++ accesoDatos/conexion.php
@@ -1,5 +1,5 @@
 <?php
-function abrirConexion(){
+/*function abrirConexion(){
 
 try{
 
@@ -33,8 +33,7 @@ function cerrarConexion($mysqli){
     }
 
 }
-?>
-<?php
+*/
 function abrirConexion(){
 
 try{
